299f1eeb94be53b0eaaab795f48b774f125b3831
sunth2010 <sunth2010@163.com>
2025-08-28 08:50:22 +0800

    feat: update 2

--- docker-compose/update_nginx_config.sh
+++ docker-compose/update_nginx_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo mv nginx_default /etc/nginx/sites-available/default
+sudo cp nginx_default /etc/nginx/sites-available/default
 sudo nginx -t
 sudo systemctl restart nginx
 sudo systemctl status nginx
